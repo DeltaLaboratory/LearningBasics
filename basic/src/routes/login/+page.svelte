2db48844047a8bc03aff03f88e57ede631c9ccaf
DeltaLaboratory <delta@deltalab.dev>
2023-07-15 16:57:49 +0900

    cleaned server

--- basic/src/routes/login/+page.svelte
+++ basic/src/routes/login/+page.svelte
@@ -20,12 +20,10 @@
 </script>
 
 <div class="container h-full mx-auto flex flex-col justify-center items-center">
-    <div class="space-y-5 flex flex-col">
-        <input type="text" bind:value={userId} class="border-2 border-gray-300 rounded-md p-2 w-96 input" />
-        <input type="password" bind:value={password} class="border-2 border-gray-300 rounded-md p-2 w-96 input" />
-        <button class="btn" on:click={async () => {await _page_login()}}>Login</button>
-    </div>
-    <div>
-        <span>{JSON.stringify(loginResult)}</span>
+    <div class="card p-10 space-y-5 flex flex-col justify-center items-center">
+        <input type="text" placeholder="ID" bind:value={userId} class="border-2 border-gray-300 rounded-md p-2 w-96 input" />
+        <input type="password" placeholder="Password" bind:value={password} class="border-2 border-gray-300 rounded-md p-2 w-96 input" />
+        <button class="btn " on:click={async () => {await _page_login()}}>Login</button>
+        <span class="text-sm">Don’t have an account? <a href="/register" class="text-primary-400">Sign Up</a></span>
     </div>
 </div>
